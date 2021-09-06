4e8b51ab502adb82fbfb640d212ccec893ad837f
antoanparashkevov <tonkata1505@gmail.com>
2021-09-06 14:04:25 +0300

    Initialize the new Vue Project

---
+++ assets/js/components/Legend.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="p-3">
+          {{title}}
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Legend",
+  props: {
+    title:{
+      type:String,
+      required:true,
+      default:'this is a default text'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ assets/js/products.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import Products from "./pages/Products";
+
+const app = createApp(Products)
+
+app.mount('#app');
+
+// window.app = app;
