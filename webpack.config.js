4fbbbb7b228d044ae12b7d8fa20f0ab00dd4a81b
antoanparashkevov <tonkata1505@gmail.com>
2021-09-10 17:28:03 +0300

    configureCssLoader

--- webpack.config.js
+++ webpack.config.js
@@ -70,10 +70,14 @@ Encore
     .enableVueLoader(() => {
     }, {
         version: 3,
-    });
-    
+    })
 
 
+    .configureCssLoader((config) => {
+    if (!Encore.isProduction() && config.modules) {
+        config.modules.localIdentName = '[name]_[local]_[hash:base64:5]';
+    }
+    })
 
 // uncomment if you use TypeScript
 //.enableTypeScriptLoader()
@@ -89,6 +93,7 @@ Encore
 //.enableReactPreset()
 //.addEntry('admin', './assets/js/admin.js')
 
+;
 
 module.exports = Encore.getWebpackConfig();
 
