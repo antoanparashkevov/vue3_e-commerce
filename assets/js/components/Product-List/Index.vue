833e4cb6bf659c9b73be328cb1659d9f03c53e69
antoanparashkevov <tonkata1505@gmail.com>
2021-09-21 15:27:43 +0300

    Adding Loading Spinner

--- assets/js/components/Product-List/Index.vue
+++ assets/js/components/Product-List/Index.vue
@@ -1,19 +1,29 @@
 <template>
 <div class="row">
+  <div class="col-12">
+    <div class="mt-4">
+      <loading-spinner v-show="products.length === 0"/>
+    </div>
+
+  </div>
+
   <product-card
       v-for="product in products"
       :key="product['@id']"
       :product="product"
   />
+
 </div>
 </template>
 
 <script>
+import LoadingSpinner from '@/components/Loading';
 import ProductCard from './ProductCard';
 export default {
   name: 'ProductList',
   components:{
-    ProductCard
+    ProductCard,
+    LoadingSpinner
   },
   props:{
     products:{
