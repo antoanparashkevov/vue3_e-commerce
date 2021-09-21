f85835a89f9712e6157354d5d33a98ff2702bc61
antoanparashkevov <tonkata1505@gmail.com>
2021-09-21 17:51:52 +0300

    Reading Server Data and add :global variable to CSS classes

--- assets/js/components/Sidebar.vue
+++ assets/js/components/Sidebar.vue
@@ -1,40 +1,59 @@
 <template>
   <!--  like this we can make style dynamic by adding colon -->
   <!--  :style="'width: ' + collapsed ? '40px' : 'auto'" -->
-<!--  or-->
-<!--  :style="{width: collapsed? '70px' : 'auto'}"-->
-<!--  or-->
-<!--  :class="{
-    [$style.sidebar]: true,
-    [$style.collapsed]:collapsed,
-     'p-3':true,
-     'mb-5':true
-  }"-->
-<!--  OR put all things into computed property componentClasses-->
+  <!--  or-->
+  <!--  :style="{width: collapsed? '70px' : 'auto'}"-->
+  <!--  or-->
+  <!--  :class="{
+      [$style.sidebar]: true,
+      [$style.collapsed]:collapsed,
+       'p-3':true,
+       'mb-5':true
+    }"-->
+  <!--  OR put all things into computed property componentClasses-->
   <div
       :class="componentClasses"
   >
-<!--    v-if or v-show-->
+    <!--    v-if or v-show-->
     <div v-show="!collapsed">
-    <h5 class="text-center">
-      Categories
-    </h5>
-    <ul class="nav flex-column mb4">
-      <!--     we use v-for directive like this-->
-      <li
-          v-for="category in categories"
-          :key="category['@id']"
-          class="nav-item"
-      >
-        <a
-            class="nav-link"
-            :href="`/category/${category.id}`"
-        >{{ category.name }}</a>
-      </li>
+      <h5 class="text-center">
+        Categories
+      </h5>
+      <ul class="nav flex-column mb4">
+        <li
+            class="nav-item"
+        >
+          <!--          [] syntax is needed tha our key(selected) is a dynamic expression-->
+          <a
+              :class="{
+            'nav-link':true,
+              'selected': currentCategoryId === null
+              }"
+              href="/"
+          >
+            All Products
+          </a>
 
-    </ul>
+        </li>
 
-    <hr>
+        <!--     we use v-for directive like this-->
+        <li
+            v-for="category in categories"
+            :key="category['@id']"
+            class="nav-item"
+        >
+          <a
+              :class="{
+            'nav-link':true,
+              'selected':  category['@id'] === currentCategoryId,
+              }"
+              :href="`/category/${category.id}`"
+          >{{ category.name }}</a>
+        </li>
+
+      </ul>
+
+      <hr>
     </div>
 
     <div class="d-flex justify-content-end">
@@ -53,27 +72,32 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
+
 export default {
   name: 'Sidebar',
-  props:{
+  props: {
     collapsed: {
-      type:Boolean,
-      required:true,
+      type: Boolean,
+      required: true,
     }
   },
   data: () => ({
     //set it false so that the component start not collapsed
     categories: [],
+    //we could add a data and initialize it to window.currentCategoryId
+    //we replace this with computed property
+    //currentCategoryId: window.currentCategoryId,
   }),
-  mounted(){
-    axios.get('/api/categories').then((response) =>{
-      console.log(response);
-    });
+  mounted() {
+    axios.get('/api/categories')
+        .then((response) => {
+          console.log(response);
+        });
   },
   async created() {
     const response = await axios.get('/api/categories');
-    this.categories= response.data['hydra:member'];
+    this.categories = response.data['hydra:member'];
     console.log(response.data['hydra:member']);
 
   },
@@ -87,14 +111,19 @@ export default {
   //     console.log(this.componentClasses);
   //   }
   // },
-  computed:{
-    componentClasses(){
-      const classes = [this.$style.sidebar,'p-3', 'mb-5'];
+  computed: {
+    componentClasses() {
+      const classes = [this.$style.sidebar, 'p-3', 'mb-5'];
 
-      if(this.collapsed){
+      if (this.collapsed) {
         classes.push(this.$style.collapsed);
       }
       return classes;
+    },
+    //currentCategoryId is a global javaScript variable and
+    //it can be used in Vue components with window.
+    currentCategoryId() {
+      return window.currentCategoryId;
     }
   }
 };
@@ -103,11 +132,12 @@ export default {
 <style lang="scss" module>
 @import "~styles/components/light-component";
 
-.sidebar {
+//or just type :global here
+.sidebar :global {
   @include light-component;
 
   //if the element have both sidebar and collapsed, set the width to 70 px
-  &.collapsed{
+  &.collapsed {
     width: 70px;
   }
 
@@ -117,6 +147,14 @@ export default {
       //because in components/light-component i import @import '../variables/colors';
       background: $blue-component-link-hover;
       background: pink;
+    }
+
+    //we are inside of modular .sidebar class
+    //and instead [$style.selected], just type 'selected' in our template
+     li a.selected {
+      //this comes directly from /variables/colors
+      //from the light-component.scss mixin we are using
+      background: $light-component-border;
     }
   }
 }
