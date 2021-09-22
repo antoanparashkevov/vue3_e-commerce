0837090df5cff5dba1bc1201ae77a4ed1ad69949
antoanparashkevov <tonkata1505@gmail.com>
2021-09-22 15:48:16 +0300

    Try/Catch an error while we are making an AJAX calls and smarter loading

--- assets/js/components/Catalog.vue
+++ assets/js/components/Catalog.vue
@@ -1,63 +1,91 @@
 <!--to see the data from for example api/products endpoint in json format through the browser, type -> 127.0.0.1:8000/api/products.jsonld -->
 <template>
-<section>
-  <div class="row">
-    <div class="col-12">
-      <h1>
-        Products
-      </h1>
+  <section>
+    <div class="row">
+      <div class="col-12">
+        <h1>
+          Products
+        </h1>
+      </div>
     </div>
-  </div>
-  <product-list :products="products"></product-list>
-  <div class="row">
-    <legend-component :title="legend + ' this is a javascript'"></legend-component>
-  </div>
-</section>
+    <product-list
+        :products="products"
+        :loading="loading"
+    ></product-list>
+    <div class="row">
+      <legend-component :title="legend + ' this is a javascript'"></legend-component>
+    </div>
+  </section>
 </template>
 
 <script>
 import ProductList from './Product-List/Index';
 import axios from 'axios';
 import LegendComponent from '@/components/Legend';
+
 export default {
   name: 'Catalog',
-  components:{
+  components: {
     LegendComponent,
     ProductList
   },
   data: () => ({
     // firstName: 'Antoan',
-    legend:'Antoan,',
-    products:[],
+    legend: 'Antoan,',
+    products: [],
+    //loading is set to false, because loading is by default false
+    loading: false,
   }),
-  props:{
-    currentCategoryId:{
-      type:String,
-      default:null,
-      require:true
+  props: {
+    currentCategoryId: {
+      type: String,
+      default: null,
+      require: true
     }
   },
-  mounted(){
-    axios.get('/api/products').then((response) => {
-     console.log(response);
+  mounted() {
+    axios.get('/api/products')
+        .then((response) => {
+          console.log(response);
 
-    })
+        });
   },
+  //THIS CREATED FUNCTION TAKES CARE OF MAKING AN AJAX REQUEST. This means that this function knows whether or not we are making an ajax calls to fetch products.
   async created() {
     //to make each category to show the products related to id
     //this will hold all the query parameters that we want to send
     const params = {};
 
-        if(this.currentCategoryId){
-          params.category=this.currentCategoryId;
-        }
+    if (this.currentCategoryId) {
+      params.category = this.currentCategoryId;
+    }
 
-        //to pass that params to axios, we add a second argument which is an option object
-    const response = await axios.get('/api/products',{
-      params
-    });
-    this.products= response.data['hydra:member'];
+    //here loading is true because we are going to fetch the products
+    this.loading = true;
+
+    //this simply declares the variable outside of try/catch scope so this response variable is available in the entire created function
+    let response;
+
+    //TRY and CATCH is for error handling if AJAX calls fails
+    try {
+      //to pass that params to axios, we add a second argument which is an option object
+      response = await axios.get('/api/products', {
+        params
+      });
+      //here loading is false because we were fetched the products at all
+      this.loading = false;
+
+    } catch (error) {
+      //when we are in this case and ajax call fails, the loading spinner will stop
+      this.loading = false;
+
+      //so if we hit the catch error, just return ( EXIT )
+      return;
+    }
+    //move this line outside of try/catch
+    this.products = response.data['hydra:member'];
     console.log(response.data['hydra:member']);
+
   },
 };
 </script>
--- assets/js/components/Product-List/Index.vue
+++ assets/js/components/Product-List/Index.vue
@@ -1,34 +1,49 @@
 <template>
-<div class="row">
-  <div class="col-12">
-    <div class="mt-4">
-      <loading-spinner v-show="products.length === 0"/>
+  <div class="row">
+    <div class="col-12">
+      <div class="mt-4">
+<!--        in reason that we are checking if products.length === 0, loading spinner is loading forever even when there are no products in the category-->
+        <loading-spinner v-show="loading"/>
+      </div>
+
+      <!--     To print error message that will show the empty category products-->
+      <h5 class="text-danger"
+      v-show="!loading && products.length === 0"
+      >
+        No products found on this category!
+      </h5>
+
     </div>
 
+    <product-card
+        v-for="product in products"
+        v-show="!loading"
+        :key="product['@id']"
+        :product="product"
+
+    />
+
   </div>
-
-  <product-card
-      v-for="product in products"
-      :key="product['@id']"
-      :product="product"
-  />
-
-</div>
 </template>
 
 <script>
 import LoadingSpinner from '@/components/Loading';
 import ProductCard from './ProductCard';
+
 export default {
   name: 'ProductList',
-  components:{
+  components: {
     ProductCard,
     LoadingSpinner
   },
-  props:{
-    products:{
-      type:Array,
-      required:true
+  props: {
+    products: {
+      type: Array,
+      required: true
+    },
+    loading: {
+      type: Boolean,
+      required: true,
     }
   }
 };
