8137c89b18870d9f1d4605928398101db4706d6b
antoanparashkevov <tonkata1505@gmail.com>
2021-10-13 19:13:36 +0300

    Make title component for showing the current category name for each list

---
+++ assets/js/components/Title.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="$style.sidebar">
+    <h1>
+      {{categoryName}}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props:{
+    currentCategoryId:{
+      type:String,
+      default:null
+    },
+    categories:{
+      type:Array,
+      required:true
+    }
+  },
+  computed:{
+    categoryName(){
+      if(currentCategoryId === null){
+        return "All products"
+      }
+        const category = this.categories.find((cat)=>(cat['@id'] === this.currentCategoryId));
+
+        return category ? category.name : '';
+
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped module>
+.sidebar {
+  h1 {
+    font-size: 1.7rem;
+  }
+}
+</style>
