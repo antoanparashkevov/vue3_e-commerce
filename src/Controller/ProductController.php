ecd7279150d1906063f6921a468b0445c3090a12
antoanparashkevov <tonkata1505@gmail.com>
2021-10-13 18:00:32 +0300

    Adding 'categories' variable available into twig file

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use ApiPlatform\Core\Api\IriConverterInterface;
 use ApiPlatform\Core\Bridge\Symfony\Routing\IriConverter;
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,9 +17,13 @@ class ProductController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('product/index.html.twig');
+        return $this->render('product/index.html.twig',[
+//            that will query all categories
+//        now we have categories variable available in our twig file
+            'categories' => $categoryRepository->findAll()
+        ]);
     }
 
     /**
@@ -27,10 +32,17 @@ class ProductController extends AbstractController
 
 //    @id is known as IRI. This is a string who show the URL for the specific product in database.
 //      This is different between the id property who is integer and created by us.
-    public function showCategory(Category $category, IriConverterInterface $iriConverter): Response
+    public function showCategory(
+        Category $category,
+        IriConverterInterface $iriConverter,
+        CategoryRepository $categoryRepository
+    ): Response
     {
         return $this->render('product/index.html.twig',[
-            'currentCategoryId' => $iriConverter->getIriFromItem($category)
+
+            'currentCategoryId' => $iriConverter->getIriFromItem($category),
+
+            'categories' => $categoryRepository->findAll()
         ]);
     }
 
